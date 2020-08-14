07c0bcb737a2e40a47478e69fa151aea19ad6df3
Donnukrit Satirakul <kalima007c@gmail.com>
2020-08-14 17:39:02 +0700

    add cert image

--- src/components/Certifcation/Certificate.tsx
+++ src/components/Certifcation/Certificate.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {Divider, Carousel} from 'antd'
+import {Divider, Carousel} from 'antd';
+import NSC2 from '../../assets/IMG_0002.jpg';
+import NSC1 from '../../assets/IMG_0005.jpg';
 import './cert.css'
 class Certificate extends Component {
     render(){
@@ -8,10 +10,9 @@ class Certificate extends Component {
                 <div><Divider style={{fontWeight:'bold',fontSize:'28px'}}>Certificate</Divider></div>
                 <div className='carousel-flex'>
                 <Carousel autoplay className='cert-carousel'>
-                    <div>Coming Soon!</div>
-                    <div>Coming Soon!</div>
-                    <div>Coming Soon!</div>
-                    <div>Coming Soon!</div>
+                    <div ><img src={NSC1} className="img"/></div>
+                    <div ><img src={NSC2} className="img"/></div>
+                    
                 </Carousel>
                 </div>
             </div>
